3f56326f0bd507b5081bddc2a7bb55425996dc5b
papiani <joshianiruddha321@gmail.com>
2024-09-23 21:45:11 +0800

    added general structure

---
+++ stakeholders/household.py
@@ -0,0 +1,23 @@
+## PERSON
+'''
+Attributes:
+0. ID
+1. gender
+2. age
+3. life expectancy (randomise based on healthcare quality)
+4. assets (seperate class ... liquidity scale also implemented)
+5. Happiness (reward parameter)
+6. education (affects earn-rate, based on skills in demand)
+7. Parents
+
+Methods:
+'''
+## BRAIN OF PERSON
+# This holds randomized parameters on behavioural/intrinsic tendencies of each human.
+# These are intrinsic values of a person, acting as a multiplier on top of market conditions/ general consumer sentiment.
+'''
+Weights:
+1. risk tolerance -> job-hopping / investment vs saving
+2. Consumption utility -> happiness gained from spending
+3. work ethic -> affects gradient of dimishing returns from work
+'''
