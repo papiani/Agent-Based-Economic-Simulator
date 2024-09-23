39d3f3ad2c78810683a5bbeb3bb8126d2686fac1
papiani <joshianiruddha321@gmail.com>
2024-09-23 20:27:44 +0800

    Added License and households.py

---
+++ Agents/household.py
@@ -0,0 +1,22 @@
+'''
+Attributes of a person in an economy:
+1. gender
+2. age
+3. life expectancy (randomise based on healthcare quality)
+4. assets (seperate class ... liquidity scale also implemented)
+5. Happiness (reward parameter)
+6. education (affects earn-rate, based on skills in demand)  
+'''
+'''
+Methods of a person in an economy and their effects:
+
+'''
+'''
+BRAIN OF PERSON
+This holds randomized parameters on behavioural/intrinsic tendencies of each human.
+These are intrinsic values of a person, acting as a multiplier on top of market conditions/ general consumer sentiment.
+Weights:
+risk tolerance -> job-hopping / investment vs saving
+Consumption utility -> 
+
+'''
